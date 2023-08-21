e7d2a2f6595cbb8555bb1f0f596f70ce3d8f7b73
Bruno Castagnola <78988774+Brunocasta@users.noreply.github.com>
2023-08-21 17:39:51 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion_db=mysqli_connect("localhost","root","","cursophpint") or 
+$conexion_db=mysqli_connect("localhost","id21066940_pablo_canonico_user","EstoEsUnaPass2341!","id21066940_pablo_canonico") or 
             exit ("No se puede conectar");
 
 
